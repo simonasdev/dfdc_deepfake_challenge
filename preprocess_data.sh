3c2dbf2fce7fdde7b21c17a7625021f5ffe7ac6a
Simonas Gildutis <simonasgildutis@gmail.com>
2020-10-23 12:42:29 +0300

    skip face detection for preprocessing

--- preprocess_data.sh
+++ preprocess_data.sh
@@ -1,6 +1,6 @@
 DATA_ROOT=$1
-echo "Extracting bounding boxes from original videos"
-PYTHONPATH=. python preprocessing/detect_original_faces.py --root-dir $DATA_ROOT
+# echo "Extracting bounding boxes from original videos"
+# PYTHONPATH=. python preprocessing/detect_original_faces.py --root-dir $DATA_ROOT
 
 echo "Extracting crops as pngs"
 PYTHONPATH=. python preprocessing/extract_crops.py --root-dir $DATA_ROOT --crops-dir crops
